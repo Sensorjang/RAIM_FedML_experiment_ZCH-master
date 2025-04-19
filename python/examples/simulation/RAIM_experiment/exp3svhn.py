7e7d46a9e9beb7985eeb880615fb013803a3daff
Sensorjang <515310624@qq.com>
2025-04-19 21:00:09 +0800

    实验3、4分不同数据集执行

---
+++ python/examples/simulation/RAIM_experiment/exp3svhn.py
@@ -0,0 +1,73 @@
+from raim_torch_hierarchicalfl_step_by_step_exp import raim
+from raim_rs_torch_hierarchicalfl_step_by_step_exp import raim_rs
+import openpyxl
+import time
+import argparse
+import matplotlib.pyplot as plt
+
+excel_save_path = "D:\\"
+
+# 绘图数据
+# figure: 训练轮次的增加，模型的精度和损失的变化
+raim_acc = []
+raim_loss = []
+raimrs_acc = []
+raimrs_loss = []
+
+# 创建一个新的Excel工作簿
+wb = openpyxl.Workbook()
+
+if __name__ == '__main__':
+    # 解析命令行参数
+    # parser = argparse.ArgumentParser(description="Process some integers.")
+    # parser.add_argument('--dataset', type=str, help='Dataset name')
+    # args = parser.parse_args()
+    # datasetname = args.dataset
+    datasetname = 'SVHN'
+
+    # 创建工作表
+    figxls = wb.create_sheet('Figure' + '_' + datasetname)
+    figxls.append(['round_idx', '[RAIM] ACC', '[RAIM] LOSS', '[RAIM-RS] ACC', '[RAIM-RS] LOSS'])
+
+    esnum = 20
+    ednum = 70
+
+    raim_acc, raim_loss = raim(False, esnum, ednum, 0.0)
+    print("[RAIM] raim_acc:{} raim_loss:{}".format(raim_acc, raim_loss))
+    raimrs_acc, raimrs_loss = raim_rs(False, esnum, ednum, 0.0)
+    print("[RAIM-RS] raimrs_acc:{} raimrs_loss:{}".format(raimrs_acc, raimrs_loss))
+
+    # 输出数据到excel表格
+    for i in range(len(raim_acc)):
+        figxls.append([i, raim_acc[i], raim_loss[i], raimrs_acc[i], raimrs_loss[i]])
+
+    wb.save(excel_save_path + 'exp3' + '_' + datasetname + '_' + str(int(time.time()))  + '.xlsx')
+
+    # 创建一个折线图，左侧 y 轴为精度，右侧 y 轴为损失，x 轴为训练轮次
+    fig1, ax1 = plt.subplots()
+    ax2 = ax1.twinx()
+
+    # 绘制 RAIM 的 ACC 和 LOSS
+    ax1.plot(raim_acc, 'b-', label='RAIM ACC')
+    ax2.plot(raim_loss, 'r--', label='RAIM LOSS')
+
+    # 绘制 RAIM-RS 的 ACC 和 LOSS
+    ax1.plot(raimrs_acc, 'g-', label='RAIM-RS ACC')
+    ax2.plot(raimrs_loss, 'm--', label='RAIM-RS LOSS')
+
+    # 设置标签和标题
+    ax1.set_xlabel('Training Round')
+    ax1.set_ylabel('Accuracy', color='b')
+    ax2.set_ylabel('Loss', color='r')
+
+    # 设置图例
+    ax1.legend(loc='upper left')
+    ax2.legend(loc='upper right')
+
+    # 保存图像
+    plt.savefig(excel_save_path + 'exp3' + '_' + datasetname + '_' + str(int(time.time())) + '.png', dpi=300)
+
+    # 显示图像
+    plt.show()
+    plt.close()
+    time.sleep(1)
---
+++ python/examples/simulation/RAIM_experiment/exp4svhn.py
@@ -0,0 +1,67 @@
+from raim_torch_hierarchicalfl_step_by_step_exp import raim
+from raim_rs_torch_hierarchicalfl_step_by_step_exp import raim_rs
+import openpyxl
+import time
+import argparse
+import matplotlib.pyplot as plt
+
+excel_save_path = "D:\\"
+
+# 绘图数据
+# figure: 在不同低声誉比例(0.1,0.2,0.3,0.4,0.5)下训练的最终精度
+raim_acc = []
+raimrs_acc = []
+
+# 创建一个新的Excel工作簿
+wb = openpyxl.Workbook()
+
+if __name__ == '__main__':
+    # 解析命令行参数
+    # parser = argparse.ArgumentParser(description="Process some integers.")
+    # parser.add_argument('--dataset', type=str, help='Dataset name')
+    # args = parser.parse_args()
+    # datasetname = args.dataset
+    datasetname = 'SVHN'
+
+    # 创建工作表
+    figxls = wb.create_sheet('Figure' + '_' + datasetname)
+    figxls.append(['lowrepu_ratio', '[RAIM] ACC', '[RAIM-RS] ACC'])
+
+    esnum = 20
+    ednum = 70
+
+    for lowrepu_ratio in [0.1, 0.2, 0.3, 0.4, 0.5]:
+        raim_acc, raim_loss = raim(False, esnum, ednum, lowrepu_ratio)
+        print("[RAIM] raim_acc:{} raim_loss:{}".format(raim_acc, raim_loss))
+        raimrs_acc, raimrs_loss = raim_rs(False, esnum, ednum, lowrepu_ratio)
+        print("[RAIM-RS] raimrs_acc:{} raimrs_loss:{}".format(raimrs_acc, raimrs_loss))
+
+        raim_acc.append(raim_acc[-1])
+        raimrs_acc.append(raimrs_acc[-1])
+
+    # 输出数据到excel表格
+    for i in range(len(raim_acc)):
+        figxls.append([0.1 * i, raim_acc[i], raimrs_acc[i]])
+
+    wb.save(excel_save_path + 'exp4' + '_' + datasetname + '_' + str(int(time.time()))  + '.xlsx')
+
+    # 创建一个柱状图， x轴轴为低声誉比例（0.1,0.2,0.3,0.4,0.5），y轴为最终精度，每个x对应两个并列不重叠的纵柱
+    fig1, ax1 = plt.subplots()
+
+    bar_width = 2  # 柱状图的宽度
+    index = [0.1, 0.2, 0.3, 0.4, 0.5]
+    index_raim = [i - bar_width / 2 for i in index]
+    index_raimrs = [i + bar_width / 2 for i in index]
+    ax1.bar(index_raim, raim_acc, width=bar_width, label='RAIM', alpha=0.7)
+    ax1.bar(index_raimrs, raimrs_acc, width=bar_width, label='RAIM-RS', alpha=0.7)
+    ax1.set_xlabel('lowrepu_ratio')
+    ax1.set_ylabel('ACC')
+    ax1.set_title('RAIM and RAIM-RS ACC')
+
+    ax1.legend()
+
+    plt.savefig(excel_save_path + 'exp4' + '_' + datasetname + '_' + str(int(time.time()))  + '.png')
+
+    plt.show()
+    plt.close()
+    time.sleep(1)
